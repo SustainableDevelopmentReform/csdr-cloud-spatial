6d0512a9ccc22ff6ea6d4c3720b85df2e218ae6a
willjnz <will.jones@hotmail.co.nz>
2025-12-05 18:58:22 +1100

    Update sedona test to calc area, not just intersect

--- csdr/sedona_test_partition.py
+++ csdr/sedona_test_partition.py
@@ -1,5 +1,7 @@
-import sedona.db
 from datetime import datetime
+
+import pandas as pd
+import sedona.db
 
 url = "s3://csdr-public-dev/datasets/aca/0-0-1/partition/"
 # s3://csdr-public-dev/datasets/aca/0-0-1/partition/reefextent_0_3.parquet
@@ -19,11 +21,21 @@ target_wkt = (
     "POLYGON ((165.3811694224928 0.7514870407407557, 165.3811694224928 -2.100780109401157, 168.79148919598953 -2.100780109401157, 168.79148919598953 0.7514870407407557, 165.3811694224928 0.7514870407407557))"
 )
 
+
 start_time = datetime.now()
-target_reef = sd.sql(f"""
-SELECT * FROM reef
-WHERE ST_Intersects(geometry, ST_SetSRID(ST_GeomFromText('{target_wkt}'), 4326))
-""").to_memtable()
+area_result = sd.sql(
+    f"""
+    SELECT SUM(ST_Area(ST_Transform(geometry, 6933))) AS total_area
+    FROM reef
+    WHERE ST_Intersects(geometry, ST_SetSRID(ST_GeomFromText('{target_wkt}'), 4326))
+    """
+).to_pandas()
+
+val = area_result['total_area'][0]
+if pd.isna(val):
+    print("No intersected reef geometries found.")
+else:
+    print(f"Total intersected area: {val:.2f} m^2")
+
 total_seconds = round((datetime.now() - start_time).total_seconds(), 2)
 print(f"Total time taken: {total_seconds} seconds")
-target_reef.show()
