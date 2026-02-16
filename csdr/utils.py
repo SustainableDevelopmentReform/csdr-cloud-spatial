2ca4397d5afcc1799fb9d37e62e97d98b7e3cf21
willjnz <will.jones@hotmail.co.nz>
2026-02-16 15:13:04 +1100

    Remove unused AWS Batch code

--- csdr/utils.py
+++ csdr/utils.py
@@ -4,15 +4,11 @@ import subprocess
 import uuid
 from collections.abc import Generator
 from contextlib import contextmanager
-from typing import Any, TypedDict, cast
+from typing import Any
 
-import boto3
 import geopandas as gpd
 import pystac
-import rioxarray  # DO NOT REMOVE: Required to enable rioxarray extension for xarray (for .rio accessor and reproject)
 import rustac
-from affine import Affine
-from odc.geo.geobox import GeoBox, GeoboxTiles
 from odc.geo.geom import Geometry
 from odc.geo.xr import mask
 from odc.stac import load
@@ -23,130 +19,6 @@ from csdr.io import get_store_with_prefix_from_url, split_path_and_file_name_fro
 
 class CSDRException(Exception):
     pass
-
-class Event(TypedDict):
-    timestamp: int
-    message: str
-    ingestionTime: int
-
-# What are these grids? They are not used internally anywhere.
-WGS84GRID10 = GeoboxTiles(
-    GeoBox(
-        (1800000, 3600000), Affine(0.0001, 0.0, -180.0, 0.0, 0.0001, -90.0), "epsg:4326"
-    ),
-    (5000, 5000),
-)
-WGS84GRID30 = GeoboxTiles(
-    GeoBox(
-        (600000, 1200000), Affine(0.0003, 0.0, -180.0, 0.0, 0.0003, -90.0), "epsg:4326"
-    ),
-    (5000, 5000),
-)
-
-
-# Submit a batch job
-def submit_job(
-    job_name: str,
-    job_queue: str,
-    job_definition: str,
-    container_overrides: dict[str, Any],
-    parameters: dict[str, str],
-    multi: bool = False,
-    multi_size: int = 30,  # This is how many tiles there are in each year
-) -> str:
-    """Submit a job to AWS Batch"""
-    client = boto3.client("batch")
-    extras = {}
-    if multi:
-        extras["arrayProperties"] = {"size": multi_size}
-
-    response: dict[str, str] = client.submit_job(
-        jobName=job_name,
-        jobQueue=job_queue,
-        jobDefinition=job_definition,
-        containerOverrides=container_overrides,
-        parameters=parameters,
-        schedulingPriorityOverride=99,
-        shareIdentifier="alex",
-        retryStrategy={"attempts": 1},
-        **extras,
-    )
-    return response["jobId"]
-
-
-# Get the status of a job
-def get_job_status(job_id: str) -> str:
-    """Get the status of a job"""
-    client = boto3.client("batch")
-    response = client.describe_jobs(jobs=[job_id])
-    return cast(str, response["jobs"][0]["status"])
-
-
-def get_cloudwatch_logs(
-    job_id: str, log_group_name: str = "/aws/batch/auspatious-csdr"
-) -> Event:
-    """Get the logs for a job"""
-    client = boto3.client("batch")
-    response = client.describe_jobs(jobs=[job_id])
-    log_stream_name = response["jobs"][0]["container"]["logStreamName"]
-
-    logs_client = boto3.client("logs")
-
-    response = logs_client.get_log_events(
-        logGroupName=log_group_name, logStreamName=log_stream_name, startFromHead=True
-    )
-
-    return cast(Event, response["events"])
-
-
-def execute(year: int, tile: tuple[int, int] | None = None) -> str:
-    """Submit one or a set of jobs to AWS Batch"""
-    extra_params = []
-    if tile is not None:
-        multi = False
-        extra_params = ["--tile", ",".join([str(t) for t in tile])]
-
-    job_name = f"version-0-1-0-{year}"
-    job_queue = "normalQueue"
-    job_definition = "auspatious-csdr"
-    container_overrides = {
-        "command": [
-            "csdr-processor",
-            "--year",
-            "Ref::year",
-            "--version",
-            "Ref::version",
-            "--n-workers",
-            "Ref::n_workers",
-            "--threads-per-worker",
-            "Ref::threads_per_worker",
-            "--memory-limit",
-            "Ref::memory_limit",
-            "Ref::overwrite",
-            *extra_params,
-        ],
-        "vcpus": 16,
-        "memory": 122880,
-    }
-    parameters = {
-        "tile": "238,47",
-        "year": f"{year}",
-        "version": "0.1.0",
-        "n_workers": "4",
-        "threads_per_worker": "32",
-        "memory_limit": "100GB",
-        "overwrite": "--no-overwrite",
-    }
-
-    job_id = submit_job(
-        job_name,
-        job_queue,
-        job_definition,
-        container_overrides,
-        parameters,
-        multi=multi,
-    )
-    return job_id
 
 
 def run_command(command: list[str]) -> tuple[bool, str, str]:
@@ -193,12 +65,12 @@ def read_stacgeoparquet(dataset_url: str) -> pystac.ItemCollection:
     # This could be refactored to use rustac.DuckdbClient.search so that less data is loaded however it gets errors sometimes such as "RustacError: External error: General error: Invalid byte order"
     path, file_name = split_path_and_file_name_from_url(dataset_url)
     store = get_store_with_prefix_from_url(path)
-    
+
     async def _rustac_read() -> dict[str, Any]:
         stac_items = await rustac.read(file_name, store=store)
         print(f"Read {len(stac_items['features'])} STAC items from {dataset_url}")
         return stac_items
-    
+
     stac_items = asyncio.run(_rustac_read())
 
     return pystac.ItemCollection.from_dict(stac_items)
@@ -246,8 +118,10 @@ def xarray_calculate_area_m2(
         data = data.where(data == value)
 
     # Validate that data and geom have the same CRS.
-    target_crs = "EPSG:6933" # For consistency with all datasets and geometries
-    data = data.rio.reproject(target_crs) # TODO: Tweak parameters of reproject: resolution=desired_resolution, method='nearest', resampling=Resampling.bilinear
+    target_crs = "EPSG:6933"  # For consistency with all datasets and geometries
+    data = data.rio.reproject(
+        target_crs
+    )  # TODO: Tweak parameters of reproject: resolution=desired_resolution, method='nearest', resampling=Resampling.bilinear
     geom = geom.to_crs(target_crs)
 
     # Mask out regions outside the geometry
